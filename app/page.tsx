f0ebff5fc82ac9cac881f93962f0f8edfb518f49
Duri Chitayat <durichitayat@safeguardglobal.com>
2024-03-14 17:37:50 -0500

    Add npm i command and update link in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -14,7 +15,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{" "}SomaCode
+           By{" "}SomaCode
           </a>
         </div>
       </div>
@@ -22,11 +23,16 @@ export default function Home() {
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1] items-center">
               
           <h1 className="text-4xl">
-            Coming Soon
+            Home
           </h1>
+            
       </div>
 
-      <div></div>
+      <div>
+        <Link className="z-100" href="/login">Login</Link>
+        <Link href="/dashboard">Dashboard</Link>
+        <Link href="/game">Game</Link>
+      </div>
 
     </main>
   );
